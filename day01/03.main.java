01bd053baa03e2b3010952cfc6c605802afc1724
poojakasthu <pooja260207@gmail.com>
2026-07-10 07:57:26 +0530

    Create 03.main.java

---
+++ day01/03.main.java
@@ -0,0 +1,17 @@
+public class main{
+    public static void main(String[] args) 
+    {
+        int num1 =(byte) 100000;
+        float num2 =(int)123456389;
+        double num3 = (int)26.349826489346845;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+      
+
+    }
+}
+/*Output
+-96
+1.23456392E8
+26.0*/
