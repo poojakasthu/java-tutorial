8311174adb124bb6afdfeba9f303d08496ee5989
poojakasthu <pooja260207@gmail.com>
2026-07-10 07:46:46 +0530

    Create 01.main.java

---
+++ day01/01.main.java
@@ -0,0 +1,12 @@
+public class main{
+    public static void main(String[] args){
+        int a = 234;
+        short b = 23456;
+        long c = 2345678901234567896532345678l;
+        String num = "two";
+        float d = 56.789f;
+        double e = 23.9845179013;
+        byte = 127;
+
+    }
+}
