2e6014035d588d3b139895398a4d3b6afe45a390
poojakasthu <pooja260207@gmail.com>
2026-07-12 18:44:24 +0530

    Create bitwise.java

---
+++ day02/bitwise.java
@@ -0,0 +1,12 @@
+public class bitwise {
+    public static void main(String[] args){
+        int a =6;
+        int b =4;
+        System.out.println(a&b);//4
+        System.out.println(a|b);//6
+        System.out.println(a^b);//2
+        System.out.println(a<<b);//96
+
+    }
+
+}
