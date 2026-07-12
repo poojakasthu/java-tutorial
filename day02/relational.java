6ac37c2b8ed9a5d5d677fa33d52edbd45a582b4c
poojakasthu <pooja260207@gmail.com>
2026-07-12 18:48:33 +0530

    Create relational.java

---
+++ day02/relational.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class relational {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+
+    }
+
+}
+/*output
+8
+10
+false
+true
+false
+true
+false
+true*/
