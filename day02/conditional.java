2abc3f46a4a2070d70c0af049b51e86c3f2423fa
poojakasthu <pooja260207@gmail.com>
2026-07-12 18:53:14 +0530

    Create conditional.java

---
+++ day02/conditional.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class conditional {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        if(num1>=0){
+            System.out.println("Positive");
+        }
+        else{
+            System.out.println("Negative");
+        }
+
+    }
+}
+-----------------
+import java.util.*;
+public class conditional {
+   public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int age = scan.nextInt();
+        if(age>=18){
+            System.out.println("Eligible for vote");
+        }
+        else{
+            System.out.println("Not Eligible for vote");
+        }
+     
+    }
+}
+     
