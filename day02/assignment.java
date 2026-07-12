3e6937451d680ebea7e08080fe6e005d126738ce
poojakasthu <pooja260207@gmail.com>
2026-07-12 18:41:48 +0530

    Create assignment.java

---
+++ day02/assignment.java
@@ -0,0 +1,15 @@
+public class Assignment {
+    public static void main(String[] args){
+        int a =5;
+        int b =2;
+        System.out.println(a+=b); //7
+        System.out.println(a-=b);//5
+        System.out.println(a*=b); //10
+        System.out.println(a/=b); //5
+        System.out.println(a%=b); //1
+        System.out.println(a&b); //0
+        System.out.println(a|b); //0
+        System.out.println(a<<=b); //3
+        System.out.println(a>>=b); //1
+    }
+}
