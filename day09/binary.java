cb5bea8f17674c17f39db958146ba81bb7992427
poojakasthu <pooja260207@gmail.com>
2026-07-20 18:09:40 +0530

    Create binary.java

---
+++ day09/binary.java
@@ -0,0 +1,24 @@
+public class binarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int target =4;
+        System.out.println(search(arr,target));
+
+    }
+    static int search(int[] arr,int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > target) {
+                end = mid- 1;
+            } else if(arr[mid]<target){
+                start = mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
