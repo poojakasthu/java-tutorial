27447e3018d2a76ddcd21c85e7fa9ccd876223c8
poojakasthu <pooja260207@gmail.com>
2026-07-23 21:47:15 +0530

    Create anagramprblm.java

---
+++ day11/anagramprblm.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class anagram {
+    public static void main(String[] args){
+        String name = "listen";
+        String name1 = "silent";
+        if(name.length() != name1.length()){
+           System.out.println("not an anagram ");
+        }
+        char[] arr = name.toCharArray();
+        char[] arr1 = name1.toCharArray();
+        Arrays.sort(arr);
+        Arrays.sort(arr1);
+        if(Arrays.equals(arr,arr1)){
+            System.out.println("anagram");
+        }
+        else{
+            System.out.println("not an anagram");
+        }
+    }
+}
