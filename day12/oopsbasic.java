9c822b05aab1c89b4d67c1b06188d20b693a794c
poojakasthu <pooja260207@gmail.com>
2026-07-26 21:41:13 +0530

    Create oopsbasic.java

---
+++ day12/oopsbasic.java
@@ -0,0 +1,14 @@
+class abc{
+    int age;
+    String name;
+    void display(){
+        System.out.println("Displaying the methods ");
+
+    }
+}
+public class oopsbasic {
+    public static void main(String[] args){
+        abc objname = new abc();
+        objname.display();
+    }
+}
